b5fc35d07612e618088d330b06ce830790df2c32
Lingyan000 <120710372@qq.com>
2020-12-20 21:41:52 +0800

    🚑 bug fixs

--- lib/rules/KinhDownload/pages/operating.ts
+++ lib/rules/KinhDownload/pages/operating.ts
@@ -212,7 +212,14 @@ export function dl(
       pwd
     );
   } catch (e) {
-    setError(e.message);
+    setHomeResult({
+      data: [
+        {
+          title: `<p style="text-align:center"><font color="red">${e.message}</font></p>`,
+          col_type: "rich_text",
+        },
+      ],
+    });
   }
   if (isObject(res.data))
     if (!!res.data.dlink && !!res.data.ua) {
@@ -233,8 +240,8 @@ export function dl(
           {
             title: `${res.data.dlink}`,
             col_type: "text_1",
-            desc: "长按复制下载地址",
-            url: "toast://长按复制",
+            desc: "长按复制下载地址（复制不完整请到下载地址2）",
+            url: `${res.data.dlink}`,
           },
           {
             title: "",
@@ -262,9 +269,29 @@ export function dl(
             title: `现在云解析必须将文件转存到超级会员账号内才可进行下载每天只能转成5万个文件，希望大家可以对云解析进行提供，限速的账号也行！限速的账号也行！限速的账号也行！你无需购买，只要到哔喱哔喱，微博账号分享帖子，微信公众号账号分享帖子，这个工具即可将svip账号加入到云解析数据库中，集中的越多以后越稳定！<a href="https://ubaq.lanzous.com/iOR96jepmte">https://ubaq.lanzous.com/iOR96jepmte</a>`,
             col_type: "rich_text",
           },
+          {
+            title: "",
+            col_type: "line_blank",
+          },
+          {
+            title: "<strong>下载地址2：</strong>",
+            col_type: "rich_text",
+          },
+          {
+            title: `${res.data.dlink}`,
+            col_type: "rich_text",
+          },
         ] as HomeRuleItem[],
       });
-    } else setError("未获取到地址，可尝试重新获取");
+    } else
+      setHomeResult({
+        data: [
+          {
+            title: `<p style="text-align:center"><font color="red">未获取到地址，可尝试重新获取</font></p>`,
+            col_type: "rich_text",
+          },
+        ],
+      });
   else {
     let pArr = parseDomForArray(res.data, ".alert-primary&&p");
     let dlink: string = "";
@@ -299,8 +326,8 @@ export function dl(
           {
             title: `${dlink}`,
             col_type: "text_1",
-            desc: "长按复制下载地址",
-            url: "toast://长按复制",
+            desc: "长按复制下载地址（复制不完整请到下载地址2）",
+            url: `${dlink}`,
           },
           {
             title: "",
@@ -328,8 +355,28 @@ export function dl(
             title: `现在云解析必须将文件转存到超级会员账号内才可进行下载每天只能转成5万个文件，希望大家可以对云解析进行提供，限速的账号也行！限速的账号也行！限速的账号也行！你无需购买，只要到哔喱哔喱，微博账号分享帖子，微信公众号账号分享帖子，这个工具即可将svip账号加入到云解析数据库中，集中的越多以后越稳定！<a href="https://ubaq.lanzous.com/iOR96jepmte">https://ubaq.lanzous.com/iOR96jepmte</a>`,
             col_type: "rich_text",
           },
+          {
+            title: "",
+            col_type: "line_blank",
+          },
+          {
+            title: "<strong>下载地址2：</strong>",
+            col_type: "rich_text",
+          },
+          {
+            title: `${dlink}`,
+            col_type: "rich_text",
+          },
         ] as HomeRuleItem[],
       });
-    } else setError("未获取到地址，可尝试重新获取");
+    } else
+      setHomeResult({
+        data: [
+          {
+            title: `<p style="text-align:center"><font color="red">未获取到地址，可尝试重新获取</font></p>`,
+            col_type: "rich_text",
+          },
+        ],
+      });
   }
 }
